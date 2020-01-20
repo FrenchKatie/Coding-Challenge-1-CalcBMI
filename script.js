6a05bccbdd58e64dbc7498099e14137dfef6d69e
Katie French <katiefrench@Katies-MacBook-Pro.local>
2020-01-20 13:50:33 +1300

    instructions added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*
+Mark and John are trying to compare their BMI, which is calcuated using this formula:
+BMI = mass / height^2 = mass / (height * height).
+(mass in KG, height in Metre).
+
+1. Store Mark and John's height and mass in variables.
+2. Calculate both their BMI's.
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Marks BMI higher than John's? True").
+*/
